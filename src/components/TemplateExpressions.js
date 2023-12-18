1d1642adf60de40695a95fd3dcedbb113bb79265
Beatriz <beatrizsouzasantos1288@gmail.com>
2023-12-18 18:21:17 -0300

    adicionando mais componentes

---
+++ src/components/TemplateExpressions.js
@@ -0,0 +1,13 @@
+const TemplateExpressions = () => {
+
+    const name = "Beatriz";
+
+    return (
+        <div>
+          <h1>Olá {name}, tudo bem?</h1>
+        </div>
+    )
+
+}
+
+export default TemplateExpressions
